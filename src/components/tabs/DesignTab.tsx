81df4429e860fff688cc4429ab7bbc5edf90e688
Angshuman Bardhan <bardhanangshu@gmail.com>
2025-11-24 21:39:31 +0530

    Design tab updated

--- src/components/tabs/DesignTab.tsx
+++ src/components/tabs/DesignTab.tsx
@@ -1,72 +1,29 @@
-import { ImageWithFallback } from '../ImageFallBack/ImageWithFallback';
-import { getIcon } from '../../utils/iconMap';
-import designsData from '../../data/designs.json';
-import '../../styles/DesignTab.css';
+import { ImageWithFallback } from "../ImageFallBack/ImageWithFallback";
+import designsData from "../../data/designs.json";
+import "../../styles/DesignTab.css";
 
 export function DesignTab() {
-  const CalendarIcon = getIcon('Calendar');
-  const FileTextIcon = getIcon('FileText');
-  const ExternalLinkIcon = getIcon('ExternalLink');
-
   return (
     <div className="designs-container">
       <div className="designs-content">
         <div className="designs-header">
           <h2 className="designs-title">{designsData.title}</h2>
-          <p className="designs-description">
-            {designsData.description}
-          </p>
+          <p className="designs-description">{designsData.description}</p>
         </div>
 
         <div className="designs-grid">
           {designsData.designs.map((design, index) => (
             <div key={index} className="design-card">
-              <div className="design-image">
+              <div className="design-image-wrapper">
                 <ImageWithFallback
                   src={design.image}
                   alt={design.title}
-                  className="design-thumbnail"
+                  className="design-image"
                 />
-                <div className="design-category">
-                  {design.category}
-                </div>
-                <div className="design-date">
-                  <CalendarIcon size={12} />
-                  {design.date}
-                </div>
               </div>
-
-              <div className="design-content">
+              <div className="design-info">
                 <h3 className="design-title">{design.title}</h3>
-                
                 <p className="design-description">{design.description}</p>
-
-                <div className="design-deliverables">
-                  <div className="deliverables-label">
-                    <FileTextIcon size={12} />
-                    Deliverables
-                  </div>
-                  <div className="deliverables-list">
-                    {design.deliverables.map((item, itemIndex) => (
-                      <span key={itemIndex} className="deliverable-tag">
-                        {item}
-                      </span>
-                    ))}
-                  </div>
-                </div>
-
-                <div className="designs-tools">
-                  {design.tools.map((tool, toolIndex) => (
-                    <span key={toolIndex} className="tool-tag">
-                      {tool}
-                    </span>
-                  ))}
-                </div>
-
-                <button className="design-details-btn">
-                  <span>View Details</span>
-                  <ExternalLinkIcon size={14} />
-                </button>
               </div>
             </div>
           ))}
@@ -75,4 +32,3 @@ export function DesignTab() {
     </div>
   );
 }
-
